3f9eb4db6f94ec9a16cbcd6536a3a271547d1bed
なおや <naoya@naoyas-Mac.local>
2017-09-21 06:51:47 +0900

    GPUIMAGEの追加

--- BpmMovieCreater/MakeViewController.swift
+++ BpmMovieCreater/MakeViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MediaPlayer
 import Photos
+import GPUImage
 
 class MakeViewController: UIViewController {
   
@@ -22,7 +23,7 @@ class MakeViewController: UIViewController {
   @IBOutlet weak var songLabel: UILabel!
   // 曲の長さ
   @IBOutlet weak var lengthLabel: UILabel!
-
+  
   // メディアピッカー
   let mediaPicker: MPMediaPickerController = MPMediaPickerController(mediaTypes: .music)
   // プレイヤー
@@ -67,11 +68,11 @@ class MakeViewController: UIViewController {
     let fetchOptions = PHFetchOptions();
     
     var date:NSDate = NSDate();
-    date = NSDate(timeIntervalSinceNow: -30*24*60*60);//1年前
+    date = NSDate(timeIntervalSinceNow: -30*24*60*60);//1 month ago
     
     fetchOptions.predicate = NSPredicate(format: "creationDate > %@", date);
     fetchOptions.sortDescriptors =  [NSSortDescriptor(key: "creationDate", ascending: false)];
-    var assets = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+    let assets = PHAsset.fetchAssets(with: .image, options: fetchOptions)
     print(assets.debugDescription);
     
     var i = 0
@@ -80,31 +81,267 @@ class MakeViewController: UIViewController {
       if(i == self.sozaiAmount){
         return
       }
-      if obj is PHAsset
-      {
-        let asset:PHAsset = obj as PHAsset;
-        let requestOptions = PHImageRequestOptions()
-        requestOptions.resizeMode = .exact
-        requestOptions.deliveryMode = .highQualityFormat
+      
+      let asset:PHAsset = obj as PHAsset;
+      let requestOptions = PHImageRequestOptions()
+      requestOptions.resizeMode = .exact
+      requestOptions.deliveryMode = .highQualityFormat
+      
+      // this one is key
+      requestOptions.isSynchronous = true
+      
+      // imageを取得
+      var imageArray: [UIImage] = [UIImage]()
+      var filterImage = UIImage()
+      PHImageManager.default().requestImage(for: asset as! PHAsset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.aspectFill, options: requestOptions, resultHandler: { (pickedImage, info) in
         
-        // this one is key
-        requestOptions.isSynchronous = true
-        
-        // imageを取得
-        var imageArray: [UIImage] = [UIImage]()
-        PHImageManager.default().requestImage(for: asset as! PHAsset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.aspectFill, options: requestOptions, resultHandler: { (pickedImage, info) in
-          
-          // フィルター定義
-//          let toonFilter = MonochromeFilter()
-//          filterImage = pickedImage!.filterWithOperation(toonFilter)
-          imageArray.append(pickedImage!)
-        })
-
-      }
+        // フィルター定義
+        let toonFilter = MonochromeFilter()
+        filterImage = pickedImage!.filterWithOperation(toonFilter)
+        imageArray.append(filterImage)
+      })
+      
       i = i + 1
     });
+//    kokokara
+//    // TODO 取得した画像をスライドショーにする
+//    let movie = MovieCreator()
+//    
+//    // TODO 画像の解像度を写真によって変更させる
+//    let movieSize = CGSize(width: 1920, height:1280)
+//    //    let movieSize = PHImageManagerMaximumSize
+//    
+//    let time = 100
+//    
+//    // 選択した写真や動画を全て5秒に設定して、mp4ファイルに変換する
+//    let movieURL = self.mergeMovie(firstMoviePathUrl: urlArray[0] as NSURL, secondMoviePathUrl: urlArray[1] as NSURL)
+//    
+//    // 音声と動画のマージ
+//    let goodMovieURL = self.mergeAudio(audioURL: audioPlayer?.url! as! NSURL, moviePathUrl: movieURL as NSURL)
+//    
+//    // ライブラリへの保存
+//    var alertController = UIAlertController(title: "完了", message: "カメラロールに保存しました", preferredStyle: .actionSheet)
+//    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//    PHPhotoLibrary.shared().performChanges({
+//      PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: goodMovieURL)
+//    }) { completed, error in
+//      if !completed {
+//        alertController = UIAlertController(title: "失敗", message: "カメラロールの保存に失敗しました", preferredStyle: .actionSheet)
+//      }
+//    }
+//    dismiss(animated: true,completion: nil)
+//    alertController.addAction(defaultAction)
+//    present(alertController, animated: true, completion: nil)
   }
+  
+  // 動画と動画のマージ
+  func mergeMovie(firstMoviePathUrl: NSURL, secondMoviePathUrl: NSURL) -> URL {
+    
+    // Compositionを生成
+    let mutableComposition: AVMutableComposition = AVMutableComposition()
+    
+    // AVAssetをURLから取得
+    let videoAssetFirst = AVAsset(url: firstMoviePathUrl as URL)
+    let videoAssetSecond = AVAsset(url: secondMoviePathUrl as URL)
+    
+    // AVAssetから動画のAVAssetTrackをそれぞれ取得
+    let videoTrackFirst = videoAssetFirst.tracks(withMediaType: AVMediaTypeVideo)[0]
+    let videoTrackSecond = videoAssetSecond.tracks(withMediaType: AVMediaTypeVideo)[0]
+    
+    // 動画合成用のAVMutableCompositionTrackを生成
+    let compositionVideoTrack = mutableComposition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
+    
+    // 動画をトラックに追加
+    do {
+      try compositionVideoTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, videoTrackFirst.timeRange.duration), of: videoTrackFirst, at: kCMTimeZero)
+    } catch {
+      fatalError("videoTrack error")
+    }
+    do {
+      try compositionVideoTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, videoTrackSecond.timeRange.duration), of: videoTrackSecond, at: kCMTimeZero)
+    } catch {
+      fatalError("videoTrack error")
+    }
+    
+    // First合成命令用オブジェクトを生成
+    let mutableVideoCompositionInstructionFirst = AVMutableVideoCompositionInstruction()
+    mutableVideoCompositionInstructionFirst.timeRange = CMTimeRangeMake(kCMTimeZero, videoTrackFirst.timeRange.duration)
+    mutableVideoCompositionInstructionFirst.backgroundColor = UIColor.red.cgColor
+    
+    // Second合成命令用オブジェクトを生成
+    let mutableVideoCompositionInstructionSecond = AVMutableVideoCompositionInstruction()
+    mutableVideoCompositionInstructionSecond.timeRange = CMTimeRangeMake(kCMTimeZero, videoTrackSecond.timeRange.duration)
+    mutableVideoCompositionInstructionSecond.backgroundColor = UIColor.red.cgColor
+    
+    // AVMutableVideoCompositionを生成
+    let mutableVideoComposition = AVMutableVideoComposition.init()
+    mutableVideoComposition.instructions = [mutableVideoCompositionInstructionFirst, mutableVideoCompositionInstructionSecond]
+    
+    // 動画の回転情報を取得する
+    let transform1 = videoTrackFirst.preferredTransform;
+    let isVideoAssetPortrait = ( transform1.a == 0 &&
+      transform1.d == 0 &&
+      (transform1.b == 1.0 || transform1.b == -1.0) &&
+      (transform1.c == 1.0 || transform1.c == -1.0));
+    
+    // 動画のサイズ設定
+    var naturalSizeFirst = CGSize()
+    var naturalSizeSecond = CGSize()
+    if (isVideoAssetPortrait) {
+      naturalSizeFirst = CGSize(width: videoTrackFirst.naturalSize.height, height: videoTrackFirst.naturalSize.width);
+      naturalSizeSecond = CGSize(width: videoTrackSecond.naturalSize.height, height: videoTrackSecond.naturalSize.width);
+      
+    }else {
+      naturalSizeFirst = videoTrackFirst.naturalSize;
+      naturalSizeSecond = videoTrackSecond.naturalSize;
+    }
+    let renderWidth  = max(naturalSizeFirst.width, naturalSizeSecond.width)
+    let renderHeight = max(naturalSizeFirst.height, naturalSizeSecond.height)
+    
+    // 書き出す動画のサイズ設定
+    mutableVideoComposition.renderSize = CGSize(width: renderWidth, height: renderHeight);
+    // 書き出す動画のフレームレート（30FPS）
+    mutableVideoComposition.frameDuration = CMTimeMake(1, 30);
+    
+    // AVMutableCompositionを元にExporterの生成
+    let assetExportSession: AVAssetExportSession = AVAssetExportSession(asset: mutableComposition, presetName: AVAssetExportPreset1920x1080)!
+    let composedMovieDirectory = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0];
+    let composedMoviePath      = NSString(format:"%@/%@", composedMovieDirectory, "test.mp4");
+    
+    // すでに合成動画が存在していたら消す
+    let fileManager = FileManager.default;
+    if (fileManager.fileExists(atPath: composedMoviePath as String)) {
+      do {
+        try fileManager.removeItem(atPath: composedMoviePath as String)
+      } catch {
+        fatalError("removeItem file")
+      }
+    }
+    
+    // 保存設定
+    let composedMovieUrl = NSURL(fileURLWithPath:composedMoviePath as String);
+    assetExportSession.outputFileType              = AVFileTypeQuickTimeMovie;
+    assetExportSession.outputURL                   = composedMovieUrl as URL;
+    assetExportSession.shouldOptimizeForNetworkUse = true;
+    
+    // エクスポート
+    assetExportSession.exportAsynchronously(completionHandler: {() -> Void in
+      switch assetExportSession.status {
+      case .completed:
+        print("Crop Success! Url -> \(composedMovieUrl)")
+      case .failed, .cancelled:
+        print("error = \(String(describing: assetExportSession.error))")
+      default:
+        print("error = \(String(describing: assetExportSession.error))")
+      }
+    })
+    
+    return composedMovieUrl as URL
+    
+  }
+  
+  // 音声と動画をマージする
+  func mergeAudio(audioURL: NSURL, moviePathUrl: NSURL) -> URL {
+    
+    // Compositionを生成
+    let mutableComposition = AVMutableComposition()
+    
+    // AVAssetをURLから取得
+    let videoAsset = AVAsset(url: moviePathUrl as URL)
+    let audioAsset = AVAsset(url: audioURL as URL)
+    
+    // AVAssetから動画・音声のAVAssetTrackをそれぞれ取得
+    let videoTrack = videoAsset.tracks(withMediaType: AVMediaTypeVideo)[0]
+    let audioTrack = audioAsset.tracks(withMediaType: AVMediaTypeAudio)[0]
+    
+    // 動画合成用のAVMutableCompositionTrackを生成
+    let compositionVideoTrack = mutableComposition.addMutableTrack(withMediaType: AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
+    let compositionAudioTrack = mutableComposition.addMutableTrack(withMediaType: AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+    
+    // 動画をトラックに追加
+    do {
+      try compositionVideoTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, videoTrack.timeRange.duration), of: videoTrack, at: kCMTimeZero)
+    } catch {
+      fatalError("videoTrack error")
+    }
+    
+    // 音声をトラックに追加
+    do {
+      try compositionAudioTrack.insertTimeRange(CMTimeRangeMake(kCMTimeZero, audioTrack.timeRange.duration), of: audioTrack, at: kCMTimeZero)
+    } catch {
+      fatalError("audioTrack error")
+    }
+    
+    // 合成命令用オブジェクトを生成
+    let mutableVideoCompositionInstruction = AVMutableVideoCompositionInstruction()
+    mutableVideoCompositionInstruction.timeRange = CMTimeRangeMake(kCMTimeZero, videoTrack.timeRange.duration)
+    mutableVideoCompositionInstruction.backgroundColor = UIColor.red.cgColor
+    
+    // AVMutableVideoCompositionを生成
+    let mutableVideoComposition = AVMutableVideoComposition.init()
+    mutableVideoComposition.instructions = [mutableVideoCompositionInstruction]
+    
+    // 動画の回転情報を取得する
+    let transform1 = videoTrack.preferredTransform;
+    let isVideoAssetPortrait = ( transform1.a == 0 &&
+      transform1.d == 0 &&
+      (transform1.b == 1.0 || transform1.b == -1.0) &&
+      (transform1.c == 1.0 || transform1.c == -1.0));
+    
+    // 動画のサイズ設定
+    var naturalSize = CGSize()
+    if (isVideoAssetPortrait) {
+      naturalSize = CGSize(width: videoTrack.naturalSize.height, height: videoTrack.naturalSize.width);
+    }else {
+      naturalSize = videoTrack.naturalSize;
+    }
+    let renderWidth  = naturalSize.width
+    let renderHeight = naturalSize.height
+    
+    // 書き出す動画のサイズ設定
+    mutableVideoComposition.renderSize = CGSize(width: renderWidth, height: renderHeight);
+    // 書き出す動画のフレームレート（30FPS）
+    mutableVideoComposition.frameDuration = CMTimeMake(1, 30);
+    
+    // AVMutableCompositionを元にExporterの生成
+    let assetExportSession = AVAssetExportSession(asset: mutableComposition, presetName: AVAssetExportPreset1920x1080)!
+    let composedMovieDirectory = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0];
+    let composedMoviePath      = NSString(format:"%@/%@", composedMovieDirectory, "test.mp4");
+    
+    // すでに合成動画が存在していたら消す
+    let fileManager = FileManager.default;
+    if (fileManager.fileExists(atPath: composedMoviePath as String)) {
+      do {
+        try fileManager.removeItem(atPath: composedMoviePath as String)
+      } catch {
+        fatalError("removeItem file")
+      }
+    }
+    
+    // 保存設定
+    let composedMovieUrl = NSURL(fileURLWithPath:composedMoviePath as String);
+    assetExportSession.outputFileType              = AVFileTypeQuickTimeMovie;
+    assetExportSession.outputURL                   = composedMovieUrl as URL;
+    assetExportSession.shouldOptimizeForNetworkUse = true;
+    
+    // エクスポート
+    assetExportSession.exportAsynchronously(completionHandler: {() -> Void in
+      switch assetExportSession.status {
+      case .completed:
+        print("Crop Success! Url -> \(composedMovieUrl)")
+      case .failed, .cancelled:
+        print("error = \(String(describing: assetExportSession.error))")
+      default:
+        print("error = \(String(describing: assetExportSession.error))")
+      }
+    })
+    
+    return composedMovieUrl as URL
+    
+  }
+  
 
+  
 }
 
 // メディアピッカーデリゲート
@@ -124,9 +361,9 @@ extension MakeViewController: MPMediaPickerControllerDelegate {
     // BPM取得処理
     _ = BPMAnalyzer.core.getBpmFrom(url, completion: {[weak self] (bpm) in
       self?.bpmLabel.text = bpm
-    
-    // テンポによる計算(5秒程度になるように計算)
-    switch Double(bpm)! {
+      
+      // テンポによる計算(5秒程度になるように計算)
+      switch Double(bpm)! {
       case 50..<60:
         self?.tempoLength = 60 / Double(bpm)! * 5
       case 60..<70:
